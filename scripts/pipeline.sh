0e35bdf8035d21b18588680a44313e887e9116aa
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-02-13 09:37:13 +0000

    code update

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -13,52 +13,58 @@ illumina_reads_2=$5
 
 mkdir -p results/${sample}
 
-if test ! -e results/${sample}/minimap2_nanopore_overlap1.sam.gz then
+if test ! -e results/${sample}/minimap2_nanopore_overlap1.sam.gz; then
         echo "Running minimap2 round 1"
-        cmd="minimap2 -a -x ava-ont -c -t 32 ${nanopore_assembly} ${nanopore_reads} | gzip - > results/${sample}/minimap2_nanopore_overlap1.sam.gz";  echo $cmd; $cmd
+        minimap2 -a -x ava-ont -t 32 ${nanopore_assembly} ${nanopore_reads} > results/${sample}/minimap2_nanopore_overlap1.sam; gzip results/${sample}/minimap2_nanopore_overlap1.sam.gz
+		echo "*** Minimap2 overlap round 1 completed ***"
 else
-        echo "*** Minimap2 overlap round 1 completed ***"
+        echo "*** Minimap2 overlap round 1 has already completed ***"
 fi
 
-if test ! -e results/${sample}/racon_round1_output &&  test results/${sample}/minimap2_nanopore_overlap1.sam.gz -nt results/${sample}/racon_round1_output ; then
+if test ! -e results/${sample}/racon_round1_output.fasta &&  test results/${sample}/minimap2_nanopore_overlap1.sam.gz -nt results/${sample}/racon_round1_output.fasta ; then
         echo "Running racon round 1"
-        cmd="racon --threads 32 --include-unpolished  ${nanopore_reads} results/${sample}/minimap2_nanopore_overlap1.sam.gz ${nanopore_assembly} > results/${sample}/racon_round1_output";  echo $cmd; $cmd
+        racon --threads 32 --include-unpolished  ${nanopore_reads} results/${sample}/minimap2_nanopore_overlap1.sam.gz ${nanopore_assembly} > results/${sample}/racon_round1_output.fasta
+		echo "*** Racon round 1 polishing completed ***"
 else
-        echo "*** Racon round 1 polishing completed ***"
+        echo "*** Racon round 1 polishing has already completed ***"
 fi
 
 echo "*** generating overlap information after round 1 racon ***"
 
-if test ! -e results/${sample}/minimap2_nanopore_overlap2.sam.gz && results/${sample}/racon_round1_output -nt results/${sample}/minimap2_nanopore_overlap2.sam.gz ; then
+if test ! -e results/${sample}/minimap2_nanopore_overlap2.sam.gz && test results/${sample}/racon_round1_output.fasta -nt results/${sample}/minimap2_nanopore_overlap2.sam.gz; then
         echo Running minimap2 round 2
-        cmd="minimap2 -a -x ava-ont -c -t 32 results/${sample}/racon_round1_output ${nanopore_reads} | gzip -  > results/${sample}/minimap2_nanopore_overlap2.sam.gz";  echo $cmd; $cmd
+        minimap2 -a -x ava-ont -t 32 results/${sample}/racon_round1_output.fasta ${nanopore_reads}  > results/${sample}/minimap2_nanopore_overlap2.sam; gzip results/${sample}/minimap2_nanopore_overlap2.sam.gz
+		echo "*** Minimap2 overlap round 2 completed ***"
 else
-        echo "*** Minimap2 overlap round 2 completed ***"
+        echo "*** Minimap2 overlap round 2 has already completed ***"
 fi
 
-if test ! -e results/${sample}/racon_round2_output &&  test results/${sample}/racon_round1_output -nt results/${sample}/racon_round2_output; then
+if test ! -e results/${sample}/racon_round2_output.fasta &&  test results/${sample}/minimap2_nanopore_overlap2.sam.gz -nt results/${sample}/racon_round2_output.fasta; then
         echo Running racon round 2
-        cmd="racon --threads 32 --include-unpolished ${nanopore_reads} results/${sample}/minimap2_nanopore_overlap2.sam.gz results/${sample}/racon_round1_output > results/${sample}/racon_round2_output";  echo $cmd; $cmd
+        racon --threads 32 --include-unpolished ${nanopore_reads} results/${sample}/minimap2_nanopore_overlap2.sam.gz results/${sample}/racon_round1_output.fasta > results/${sample}/racon_round2_output.fasta
+		echo "*** Racon round 2 polishing completed ***"
 else
-        echo "*** Racon round 2  polishing completed ***"
+        echo "*** Racon round 2 polishing has already completed ***"
 fi
 
 
-echo " *** generating overlap information after round 2 racon ***"
+#echo " *** generating overlap information after round 2 racon ***"
 
-if test ! -e results/${sample}/minimap3_nanopore_overlap2.sam.gz && results/${sample}/racon_round2_output -nt results/${sample}/minimap3_nanopore_overlap2.sam.gz; then
-        echo Running minimap2 round 3
-        cmd="minimap2 -a -x ava-ont -c -t 32 results/${sample}/racon_round2_output ${nanopore_reads} | gzip - > results/${sample}/minimap2_nanopore_overlap3.sam.gz";  echo $cmd; $cmd
-else
-        echo "*** Minimap2 overlap round 3 completed ***"
-fi
+#if test ! -e results/${sample}/minimap2_nanopore_overlap3.sam.gz && test results/${sample}/racon_round2_output.fasta -nt results/${sample}/minimap2_nanopore_overlap3.sam.gz; then
+#        echo Running minimap2 round 3
+#        minimap2 -a -x ava-ont -t 32 results/${sample}/racon_round2_output.fasta ${nanopore_reads} > results/${sample}/minimap2_nanopore_overlap3.sam; gzip results/${sample}/minimap2_nanopore_overlap3.sam.gz
+#		echo "*** Minimap2 overlap round 3 completed ***"
+#else
+#        echo "*** Minimap2 overlap round 3 has already completed ***"
+#fi
 
-if test ! -e results/${sample}/racon_round3_output &&  test results/${sample}/racon_round2_output -nt results/${sample}/racon_round3_output; then
-        echo Running racon round 3
-        cmd="racon --threads 32 --include-unpolished ${nanopore_reads} results/${sample}/minimap2_nanopore_overlap3.sam.gz results/${sample}/racon_round2_output > results/${sample}/racon_round3_output";  echo $cmd; $cmd
-else
-        echo " **** Racon round 3 polishing completed ***"
-fi
+#if test ! -e results/${sample}/racon_round3_output.fasta &&  test  results/${sample}/minimap2_nanopore_overlap3.sam.gz -nt results/${sample}/racon_round3_output.fasta; then
+#        echo Running racon round 3
+#        racon --threads 32 --include-unpolished ${nanopore_reads} results/${sample}/minimap2_nanopore_overlap3.sam.gz results/${sample}/racon_round2_output.fasta > results/${sample}/racon_round3_output.fasta
+#		echo " **** Racon round 3 polishing completed ***"
+#else
+#        echo " **** Racon round 3 polishing has already completed ***"
+#fi
 
 
 ## next step map illumina reads to Nanopore polished assembly from above steps
@@ -69,16 +75,34 @@ source samtools-1.9
 
 echo "*** running bowtie and pilon ***"
 
-if test ! -e results/${sample}/racon_round3_output; then
-        cmd=$(bowtie2-build results/${sample}/racon_round3_output results/${sample}/racon_round3_output);  echo $cmd; $cmd
+raconfile=racon_round2_output.fasta
+if test ! -e $raconfile; then
+        bowtie2-build -f results/${sample}/$raconfile results/${sample}/$raconfile
+	echo "*** bowtie2 build completed***"
 fi
 
-if test ! -e result/${sample}/illumina_aligned_to_assembly_sorted.bam; then
-        if [ "$illumina_reads_2" == "" ]; then
-                cmd="bowtie2 --threads 8 --qc-filter --no-unal -x results/${sample}/racon_round3_output -U $illumina_reads_1 | samtools view -b  | samtools sort -o results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam";  echo $cmd; $cmd
-                cmd="pilon --genome results/${sample}/racon_round3_output --unpaired results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam --outdir results/${sample} --output ${sample}_pilcon_polished --changes --threads 16";  echo $cmd; $cmd
-        else
-                cmd="bowtie2 --threads 8 --qc-filter --no-unal -x results/${sample}/racon_round3_output -1 $illumina_reads_1 -2 $illumina_reads_2 | samtools view -b  | samtools sort -o results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam";  echo $cmd; $cmd
-                cmd="pilon --genome results/${sample}/racon_round3_output --frags results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam --outdir results/${sample} --output ${sample}_pilcon_polished --changes --threads 16"; echo $cmd; $cmd
-        fi
+
+if [ "$illumina_reads_2" == "" ]; then
+	if test ! -e result/${sample}/illumina_aligned_to_assembly_sorted.bam; then
+        bowtie2 --threads 8 --qc-filter --no-unal -x results/${sample}/$raconfile -U $illumina_reads_1 | samtools view -b  | samtools sort -o results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam; samtools index results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam
+	else
+		echo "*** bowtie has already completed***"
+	fi
+	if test ! -e results/${sample}/${sample}_pilcon_polished; then
+		pilon --genome results/${sample}/$raconfile --unpaired results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam --outdir results/${sample} --output ${sample}_pilcon_polished --changes --threads 16
+    else
+		echo "*** Pilcon has already completed ***"
+	fi
+else
+	if test ! -e results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam; then
+		bowtie2 --threads 8 --qc-filter --no-unal -x results/${sample}/$raconfile  -1 $illumina_reads_1 -2 $illumina_reads_2 | samtools view -b  | samtools sort -o results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam; samtools index results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam;
+	else
+		echo "*** Bowtie has already completed ***"
+	fi
+
+	if test ! -e results/${sample}/${sample}_pilcon_polished; then
+		pilon --genome results/${sample}/$raconfile --frags results/${sample}/illumina_aligned_to_assembly_sorted.sorted.bam --outdir results/${sample} --output ${sample}_pilcon_polished --changes --threads 16
+	else
+		echo "*** Pilcon has already completed ***"
+    fi
 fi
