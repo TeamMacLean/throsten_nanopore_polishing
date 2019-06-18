e0f98d4d128538e2c02b0357303e01749b6347a0
TSL-RamKrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-06-18 16:37:54 +0100

    script modified

--- scripts/run_cd156_version2.sh
+++ scripts/run_cd156_version2.sh
@@ -16,7 +16,6 @@ R3=/tsl/scratch/langnert/Minichromosomes/GEMO_project/454_reads/ERR747970.fastq
 # forth read set, if you have or reverse read to second paired end reads
 R4=/tsl/scratch/langnert/Minichromosomes/GEMO_project/454_reads/ERR747971.fastq
 
-rake -f scripts/pipeline.rake sample=CD156 nanopore_reads=/tsl/scratch/langnert/Minichromosomes/GEMO_project/Nanopore_reads/CD156_FG1801_08_pass.fastq.gz assembly=/tsl/scratch/langnert/Minichromosomes/GEMO_project/Nanopore_polishing_version2/CD156/CD156_joined_28.fasta R1=/tsl/scratch/langnert/Minichromosomes/GEMO_project/454_reads/ERR747968.fastq R2=/tsl/scratch/langnert/Minichromosomes/GEMO_project/454_reads/ERR747969.fastq R3=/tsl/scratch/langnert/Minichromosomes/GEMO_project/454_reads/ERR747970.fastq R4=/tsl/scratch/langnert/Minichromosomes/GEMO_project/454_reads/ERR747971.fastq CD156
 
-rake -f scripts/pipeline.rake sample=CD156 nanopore_reads=${nanopore} assembly=${assembly} R1=${R1} R2=${R2} R3=${R3} R4=${R4} CD156
+rake -f scripts/pipeline.rake sample=CD156 nanopore_reads=${nanopore} assembly=${assembly} R1=${R1} R2=${R2} R3=${R3} R4=${R4}
 
